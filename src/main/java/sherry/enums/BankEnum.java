c9428b896c0fd7f87d129f5e794e54ad8c11df3a
th743779 <1324714606@qq.com>
2023-06-17 08:35:27 +0800

    杨哥mysql高级+基础回顾

---
+++ src/main/java/sherry/enums/BankEnum.java
@@ -0,0 +1,54 @@
+package sherry.enums;
+
+
+import lombok.Getter;
+
+import java.util.Arrays;
+
+/**
+ * @Description: 静态数据枚举: 将常用的数据存储在枚举类型中，在程序运行时直接从枚举值中获取数据，而不需要每次都查询数据库
+ * 1.举值定义
+ * 2.枚举构造
+ * 3.遍历查询
+ * 举值->构造->遍历
+ * @Author: SHERRY
+ * @email: <a href="mailto:SherryTh743779@gmail.com">TianHai</a>
+ * @Date: 2023/6/16 19:49
+ */
+public enum BankEnum
+{
+    //1.举值定义
+    CCB("ccb","中国建设银行"),
+    ABC("abc","中国农业银行");
+    //2.枚举构造
+    @Getter
+    String code;
+    @Getter
+    String message;
+    BankEnum(String code,String message){
+        this.code=code;
+        this.message=message;
+    }
+    //3.遍历查询
+    public static BankEnum getBankEnum(String code){
+        BankEnum [] bankEnums=BankEnum.values();
+        for (BankEnum element : bankEnums) {
+            if(element.getCode().equalsIgnoreCase(code)){
+                return element;
+            }
+        }
+        return null;
+    }
+    //流式遍历查询
+    public static BankEnum getBankEnumSteam(String code){
+        return Arrays.stream(BankEnum.values()).filter(x->x.getCode().equalsIgnoreCase(code)).findFirst().orElse(null);
+
+    }
+    public static void main(String[] args) {
+        BankEnum ccb = getBankEnumSteam("ccb");
+//        System.out.println(ccb.getCode());
+//        System.out.println(ccb.getMessage());
+        System.out.println(ccb);
+    }
+}
+
---
+++ src/main/java/sherry/enums/Seasons.java
@@ -0,0 +1,51 @@
+package sherry.enums;
+
+
+import lombok.Getter;
+
+/**
+ * @Description: 举值,构造,遍历
+ * @Author: SHERRY
+ * @email: <a href="mailto:SherryTh743779@gmail.com">TianHai</a>
+ * @Date: 2023/6/16 20:36
+ */
+public enum Seasons {
+//    spring,summer,fall,winter
+    SPRINMG("spring","春天的竹笋炒肉","2023"),
+    SUMMER("summer","夏天荔枝炒肉","2023"),
+    FALL("fall","秋天给你一板栗","2023"),
+    WINTER("winter","冬天的第一个起暖气","2023");
+    @Getter
+    String key;
+    @Getter
+    String message;
+    @Getter
+    String year;
+
+    //构造
+    Seasons(String code,String message,String year){
+        this.key=code;
+        this.message=message;
+        this.year=year;
+    }
+    //遍历
+    //枚举的遍历先声明方法的类型为当前创建的枚举类
+
+    public static Seasons getSeaSons (String code){
+        Seasons [] seasons=Seasons.values();
+        for (Seasons element : seasons) {
+            if(element.getKey().equalsIgnoreCase(code)){
+                return element;
+            }
+        }
+        return null;
+    }
+    public static void main(String[] args) {
+        Seasons spring = getSeaSons("winter");
+        System.out.println(spring.getKey());
+        System.out.println(spring.getMessage());
+        System.out.println(spring.year);
+        System.out.println(spring);
+    }
+
+}
